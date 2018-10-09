78e21a3ed864aa30649cd0f66ad9e70b6759cd32
vlkrb <vlkrb@genesis-mining.com>
2018-10-09 11:27:12 +0200

    added lock util classes

---
+++ src/pool/WorkQueue.h
@@ -0,0 +1,102 @@
+
+#pragma once
+
+#include <deque>
+#include <src/util/LockUtils.h>
+#include <src/util/Logging.h>
+#include <src/common/Pointers.h>
+#include <src/common/Optional.h>
+#include <src/common/Assert.h>
+
+namespace miner {
+
+    //thread safe container that can be used for queueing work and workresult unique_ptrs
+    template<class T>
+    class WorkQueue {
+
+        std::mutex mutex;
+        std::condition_variable conditionVariable;
+
+        size_t limit = 0;
+        std::deque<T> buffer;
+
+        //returns lock if work is available or nullopt on timeout
+        optional<std::unique_lock<std::mutex>> waitUntilNotEmptyAndLock(std::chrono::milliseconds timeout) {
+            std::unique_lock<std::mutex> lock(mutex);
+
+            //only start waiting if the buffer is actually empty
+            if (buffer.empty()) {
+                bool success = conditionVariable.wait_for(lock, timeout, [this] {
+                    return !buffer.empty();
+                });
+
+                if (!success) //timeout
+                    return nullopt;
+            }
+            return lock;
+        }
+
+    public:
+
+        explicit WorkQueue(size_t limit = std::numeric_limits<size_t>::max())
+        : limit(limit) {
+        }
+
+        size_t size() const {
+            std::lock_guard<std::mutex> lock(mutex);
+            return buffer.size();
+        }
+
+        bool empty() const {
+            std::lock_guard<std::mutex> lock(mutex);
+            return buffer.empty();
+        }
+
+        void clear() {
+            std::lock_guard<std::mutex> lock(mutex);
+            buffer.clear();
+        }
+
+        //push_back of an item, returns number of elements afterwards
+        size_t pushBack(T &&t) {
+            size_t size = 0;
+            {
+                std::lock_guard<std::mutex> lock(mutex);
+
+                if (buffer.size() >= limit)
+                    buffer.pop_back();
+
+                buffer.push_front(std::move(t));
+                size = buffer.size();
+            }
+            conditionVariable.notify_one();
+            return size;
+        }
+
+        optional<T> popBackTimeout() {
+            auto timeout = std::chrono::milliseconds(30);
+
+            if (auto maybeLock = waitUntilNotEmptyAndLock(timeout)) {
+                auto t {std::move(buffer.back())};
+                buffer.pop_back();
+
+                LOG(INFO) << "<-- got work";
+                return t;
+            }
+            return nullopt; //timeout
+        }
+
+        optional<T> popFrontTimeout() {
+            auto timeout = std::chrono::milliseconds(30);
+
+            if (auto maybeLock = waitUntilNotEmptyAndLock(timeout)) {
+                auto t {std::move(buffer.front())};
+                buffer.pop_front();
+                return t;
+            }
+            return nullopt; //timeout
+        }
+
+    };
+
+}
---
+++ src/util/LockUtils.h
@@ -0,0 +1,47 @@
+
+#pragma once
+
+#include <mutex>
+
+namespace miner {
+
+    template<class T>
+    class Locked {
+        std::unique_lock<std::mutex> lock;
+        std::reference_wrapper<T> t;
+
+    public:
+        Locked(std::mutex &mut, T &t)
+                : lock(mut)
+                , t(t) {
+        }
+
+        Locked(Locked &&) noexcept = default;
+        Locked &operator=(Locked &&) noexcept = default;
+
+        T *operator->() {return &t.get();}
+        T &operator *() {return  t.get();}
+        const T *operator->() const {return &t.get();}
+        const T &operator *() const {return  t.get();}
+
+        operator bool() const {return true;}
+    };
+
+    template<class T>
+    class LockGuarded {
+        T t;
+        std::mutex mut;
+
+    public:
+        template<class ... Args>
+        LockGuarded(Args &&...args)
+                : t(std::forward<Args>(args) ...) {
+        }
+
+        Locked<T> lock() {
+            return {mut, t};
+        }
+
+    };
+
+}
