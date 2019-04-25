c36875c120e1dde56611e54dc27bf1fbce3a1958
Philipp Otterbein <philipp.otterbein@genesis-mining.com>
2019-04-25 22:48:13 +0200

    simplify constructor

--- src/application/Device.cpp
+++ src/application/Device.cpp
@@ -6,7 +6,8 @@ namespace miner {
     Device::Device(const DeviceId &_id, const AlgoSettings &_settings, size_t _deviceIndex)
     : id(_id)
     , settings(_settings)
-    , deviceIndex(_deviceIndex) {
-        api = std::move(GpuApi::tryCreate(_id));
+    , deviceIndex(_deviceIndex)
+    , api(GpuApi::tryCreate(_id)) {
     }
+
 }
