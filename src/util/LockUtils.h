845980912d6d9eab6c11ffa453e79fbbdfaa4773
Philipp Otterbein <philipp.otterbein@genesis-mining.com>
2019-07-05 14:55:56 +0200

    improve LockUtils

--- src/util/LockUtils.h
+++ src/util/LockUtils.h
@@ -1,40 +1,92 @@
 
 #pragma once
 
+#include <atomic>
 #include <mutex>
+#include <shared_mutex>
+#include <src/common/Assert.h>
 #include <src/util/Copy.h>
 
 namespace miner {
 
-    template<class T>
+    template<class T, class Mutex = std::mutex>
+    class LockGuarded;
+
+    template<class T, class SharedMutex = std::shared_timed_mutex>
+    class SharedLockGuarded;
+
+    template<class T, class SharedMutex = std::shared_timed_mutex>
+    class UpgradeableLockGuarded;
+
+
+    template<class T, class Mutex = std::shared_timed_mutex>
+    class WriteLocked;
+
+    template<class T, class Mutex = std::shared_timed_mutex>
+    class ReadLocked;
+
+
+    template<class T, class Lock = std::unique_lock<typename T::mutex_type>>
     class Locked {
-        std::unique_lock<std::mutex> lock;
-        std::reference_wrapper<T> t;
+        template<class U, class M> friend class LockGuarded;
+        template<class U, class M> friend class SharedLockGuarded;
+
+    protected:
+        Lock lock;
+        std::reference_wrapper<T> guarded;
+
+        std::remove_const_t<T> &getMutableRef() const {
+            return const_cast<std::remove_const_t<T>&>(guarded.get());
+        }
+
+        std::add_const_t<T> &getConstRef() const {
+            return guarded.get();
+        }
+
+        Locked(const std::reference_wrapper<T> t)
+                : lock(t.get().mut)
+                , guarded(t) {
+            guarded.get().refCount.fetch_add(1, std::memory_order_relaxed);
+        }
 
     public:
-        Locked(std::mutex &mut, T &t)
-                : lock(mut)
-                , t(t) {
-        }
 
         Locked(Locked &&) noexcept = default;
         Locked &operator=(Locked &&) noexcept = default;
 
-        T *operator->() {return &t.get();}
-        T &operator *() {return  t.get();}
-        const T *operator->() const {return &t.get();}
-        const T &operator *() const {return  t.get();}
+        ~Locked() {
+            if (lock) {
+                guarded.get().refCount.fetch_add(-1, std::memory_order_relaxed);
+            }
+        }
 
-        operator bool() const {return true;}
+        inline operator bool() const {return lock;}
+
+        inline std::conditional_t<std::is_const<T>::value, std::add_const_t<typename T::value_type>, typename T::value_type> &operator*() {
+            MI_EXPECTS(lock);
+            return guarded.get().t;
+        }
+        inline std::conditional_t<std::is_const<T>::value, std::add_const_t<typename T::value_type>, typename T::value_type> *operator->() {return &operator*();}
+
+        inline const typename T::value_type *operator->() const {
+            MI_EXPECTS(lock);
+            return guarded.get().t;
+        }
+        inline const typename T::value_type &operator *() const {return  operator*();}
     };
 
-    template<class T>
+    template<class T, class Mutex>
     class LockGuarded {
+        template<class U, class M> friend class Locked;
+
+    protected:
         T t;
-        mutable std::mutex mut;
+        mutable std::atomic_int refCount {0};
+        mutable Mutex mut;
 
     public:
         using value_type = T;
+        using mutex_type = Mutex;
 
         DELETE_COPY_AND_MOVE(LockGuarded); //delete move, because this->t is referenced by return value of lock()
 
@@ -43,12 +95,133 @@ namespace miner {
                 : t(std::forward<Args>(args) ...) {
         }
 
-        Locked<T> lock() {
-            return {mut, t};
+        auto lock() const -> Locked<std::remove_reference_t<decltype(*this)>> {
+            return {*this};
         }
 
-        Locked<const T> lock() const {
-            return {mut, t};
+        auto lock() -> Locked<std::remove_reference_t<decltype(*this)>> {
+            return {*this};
+        }
+
+        ~LockGuarded() {
+            auto _refCount = refCount.load(std::memory_order_relaxed);
+            CHECK_EQ(_refCount, 0) << "Dangling reference to LockGuarded object detected: refCount = " << _refCount
+                                   << std::endl << el::base::debug::StackTrace();
+        }
+    };
+
+
+
+    template<class T, class SharedMutex>
+    class SharedLockGuarded : public LockGuarded<T, SharedMutex> {
+
+    public:
+
+        DELETE_COPY_AND_MOVE(SharedLockGuarded); //delete move, because this->t is referenced by return value of lock()
+
+        template<class ... Args>
+        SharedLockGuarded(Args &&...args)
+                : LockGuarded<T, SharedMutex>(std::forward<Args>(args) ...) {
+        }
+
+        Locked<const SharedLockGuarded<T, SharedMutex>, std::shared_lock<SharedMutex>> readLock() {
+            return {*this};
+        }
+
+    };
+
+
+
+    template<class T, class Mutex>
+    class ReadLocked : public Locked<const UpgradeableLockGuarded<T, Mutex>, std::shared_lock<Mutex>> {
+        template<class U, class M> friend class WriteLocked;
+        template<class U, class M> friend class UpgradeableLockGuarded;
+
+    protected:
+
+        ReadLocked(const UpgradeableLockGuarded<T, Mutex> &t)
+                : Locked<const UpgradeableLockGuarded<T, Mutex>, std::shared_lock<Mutex>>(t) {
+        }
+
+    public:
+
+        ReadLocked(ReadLocked &&) noexcept = default;
+        ReadLocked &operator=(ReadLocked &&) noexcept = default;
+
+        WriteLocked<T, Mutex> upgrade() {
+            std::lock_guard<std::mutex> lk(this->guarded.get().upgradeMut);
+            // move this->lock to new object
+            auto mut = this->lock.release();
+            mut->unlock_shared();
+            this->guarded.get().refCount.fetch_add(-1, std::memory_order_relaxed);
+            return {this->getMutableRef()};
+        }
+    };
+
+    template<class T, class Mutex>
+    class WriteLocked : public Locked<UpgradeableLockGuarded<T, Mutex>, std::unique_lock<Mutex>> {
+        template<class U, class M> friend class ReadLocked;
+        template<class U, class M> friend class UpgradeableLockGuarded;
+
+    protected:
+
+        WriteLocked(UpgradeableLockGuarded<T, Mutex> &t)
+                : Locked<UpgradeableLockGuarded<T, Mutex>, std::unique_lock<Mutex>>(t) {
+        }
+
+    public:
+
+        WriteLocked(WriteLocked &&) noexcept = default;
+        WriteLocked &operator=(WriteLocked &&) noexcept = default;
+
+        ReadLocked<T, Mutex> downgrade() {
+            std::lock_guard<std::mutex> lk(this->guarded.get().upgradeMut);
+            // move this->lock to new object
+            auto mut = this->lock.release();
+            mut->unlock();
+            this->guarded.get().refCount.fetch_add(-1, std::memory_order_relaxed);
+            return {this->getConstRef()};
+        }
+    };
+
+
+    template<class T, class SharedMutex>
+    class UpgradeableLockGuarded {
+        template<class U, class M> friend class Locked;
+        template<class U, class M> friend class WriteLocked;
+        template<class U, class M> friend class ReadLocked;
+
+    protected:
+        T t;
+        mutable std::atomic_int refCount {0};
+        mutable std::mutex upgradeMut;
+        mutable SharedMutex mut;
+
+    public:
+        using value_type = T;
+        using mutex_type = SharedMutex;
+
+        DELETE_COPY_AND_MOVE(UpgradeableLockGuarded);
+
+        template<class ... Args>
+        UpgradeableLockGuarded(Args &&...args)
+                : t(std::forward<Args>(args) ...) {
+        }
+
+        WriteLocked<T, SharedMutex> lock() {
+            std::lock_guard<std::mutex> lk(upgradeMut);
+            return {*this};
+        }
+
+        ReadLocked<T, SharedMutex> readLock() {
+            std::lock_guard<std::mutex> lk(upgradeMut);
+            return {*this};
+        }
+
+        ~UpgradeableLockGuarded() {
+            auto _refCount = refCount.load(std::memory_order_relaxed);
+            CHECK_EQ(_refCount, 0) << "Dangling reference to UpgradeableLockGuarded object detected: refCount = "
+                                   << _refCount << std::endl << el::base::debug::StackTrace();
         }
 
     };
