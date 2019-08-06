6698c95c83d024c9dbd5fdd60f047597da650521
vlkrb <vlkrb@genesis-mining.com>
2019-08-06 12:06:52 +0200

    documentation work: more jrpc docs

--- src/network/JsonRpcBuilder.h
+++ src/network/JsonRpcBuilder.h
@@ -6,7 +6,10 @@
 
 namespace miner { namespace jrpc {
 
-    class RequestBuilder { //builder pattern util for making a jrpc request
+    /**
+     * Builder pattern convenience class for creating a `jrpc::Request`
+     */
+    class RequestBuilder {
 
         Message _msg;
 
@@ -17,12 +20,38 @@ namespace miner { namespace jrpc {
     public:
         RequestBuilder();
 
+        /**
+         * set `Message::id`
+         * @return `*this` (builder pattern)
+         */
         RequestBuilder &id(nl::json id);
+
+        /**
+         * set `Request::method`
+         * @return `*this` (builder pattern)
+         */
         RequestBuilder &method(String methodName);
+
+        /**
+         * add a named param. It is not valid to add a named param if unnamed params were added before to the same `RequestBuilder`
+         * @param key name of the param
+         * @param val value of the param
+         * @return `*this` (builder pattern)
+         */
         RequestBuilder &param(const char *key, nl::json val);
+
+        /**
+         * add an unnamed param. It is not valid to add an unnamed param if named params were added before to the same `RequestBuilder`
+         * @param val value of the param
+         * @return `*this` (builder pattern)
+         */
         RequestBuilder &param(nl::json val);
 
-        Message done(); //call this at the end to convert to jrpc::Message
+        /**
+         * call this function to end the building process and `std::move` the internal `jrpc::Message` out.
+         * Afterwards, this object is in a ['valid but unspecified state'](https://en.cppreference.com/w/cpp/utility/move).
+         */
+        Message done();
     };
 
 }}
--- src/network/JsonRpcMessage.h
+++ src/network/JsonRpcMessage.h
@@ -44,8 +44,20 @@ namespace miner { namespace jrpc {
         server_error_range_last  = -32000,
     };
 
+    /**
+     * converts `j` to a `jrpc::Number`, then calls `toErrorCode(Number)`
+     * @param j
+     * @return
+     */
     ErrorCode toErrorCode(const nl::json &j);
-    ErrorCode toErrorCode(int64_t n);
+
+    /**
+     * converts `n` to a valid `ErrorCode` or `ErrorCode::error_code_unknown`. 'valid' means it's either one of the
+     * explicitly named values of `ErrorCode` or a value between `ErrorCode::server_error_range_first` and `ErrorCode::server_error_range_last`.
+     * @param n error code number as described in the [JSON-RPC 2.0 spec](https://www.jsonrpc.org/specification#error_object)
+     * @return valid `ErrorCode` or `ErrorCode::error_code_unknown`
+     */
+    ErrorCode toErrorCode(Number n);
 
     struct Error { //error-specific part of response
         ErrorCode code = error_code_unknown;
@@ -53,33 +65,92 @@ namespace miner { namespace jrpc {
         nl::json data;
     };
 
+    /**
+     * response-specific part of `jrpc::Message`. contains a variant that is either an `Error`
+     */
     struct Response { //response-specific part of message
         variant<Error, nl::json /*= Result*/> var = Error{};
     };
 
+    /**
+     * request-specific part of `jrpc::Message`. contains name of the method as well as parameters. (`params` is `nl::json{}` if the method takes no parameters)
+     */
     struct Request { //request-specific part of message
         String method;
         nl::json params;
     };
 
+    /**
+     * `jrpc::Message` class that is used to represent either a jrpc Request or Response
+     */
     struct Message {
         //constexpr static auto jsonrpc = "2.0"; //always the same
         variant<Request, Response> var = Request{};
+
+        /**
+         * `id`, ideally `Null`, `String`, `Number` or empty (=`nl::json{}`). Empty means that no `id` member is
+         * present altogether and therefore the `Message` is a notification (see `isNotification()`)
+         */
         nl::json id; //ideally Null, String or Number, empty when notification
 
         Message() = default;
+
+        /**
+         * creates a `jrpc::Message` by parsing an `nl::json`. Throws the [exceptions of nlohmann::json::at](https://nlohmann.github.io/json/classnlohmann_1_1basic__json_acac9d438c9bb12740dcdb01069293a34.html) on parsing failure.
+         */
         explicit Message(nl::json); //throws on parser failure
+
+        /**
+         * convenience ctor for creating a response `jrpc::Message` from a `jrpc::Response` and optionally an `id`.
+         * @param id empty by default, sets the `id` member of the constructed `jrpc::Message`
+         */
         explicit Message(const Response &, nl::json id = {});//convenience constructor for making a response message
+
+        /**
+         * creates a `nl::json` that corresponds to this `jrpc::Message` object. Use `jrpc::Message::str()` instead if you want to directly convert the object to `std::string`
+         */
         nl::json toJson() const;
 
+        /**
+         * whether this `Message` is a notification, that is, a `Response` with no `id` (`id == nl::json{}`)
+         */
         bool isNotification() const;
+
+        /**
+         * whether this `Message` is a `Request` as opposed to being a `Response`
+         */
         bool isRequest () const;
+
+        /**
+         * whether this `Message` is a `Response` as opposed to being a `Request`
+         */
         bool isResponse() const;
 
+        /**
+         * convenience function for quick access to the `jrpc::Error`.
+         * checks if this `Message` is a `jrpc::Response`, then checks whether it is a `jrpc::Error`. If it is, returns the ref to that `jrpc::Error`
+         * @return ref to the underlying `jrpc::Error` or `nullopt`
+         */
         optional_ref<const Error> getIfError() const;
+
+        /**
+         * convenience function for quick access to the result `nl::json` object, if this `Message` is a `Request` that is not an `Error`.
+         * @return ref to underlying result `nl::json` object, or `nullopt`
+         */
         optional_ref<nl::json> getIfResult(); //TODO: make const
+
+        /**
+         * convenience function for quick access to the `Request` object.
+         * @return ref to this `Message`'s `jrpc::Request` object if it is a request, `nullopt` otherwise.
+         */
         optional_ref<Request> getIfRequest(); //TODO: make const
 
+        /**
+         * convenience function that checks whether this `Message` is a `Response` that is not an `Error`. If so, tries to
+         * convert the `"result"` object to `T` and return it.
+         * @tparam T the type that the `"result"` should be converted to
+         * @return the `jrpc::Response`s `"result"` as a `T`, or `nullopt`
+         */
         template<class T>
         optional<T> resultAs() {
             if (optional_ref<nl::json> result = getIfResult()) {
@@ -91,17 +162,29 @@ namespace miner { namespace jrpc {
             return nullopt;
         }
 
-        //returns whether this message is a Response with a "result" = true entry
-        //this is needed so often that it deserves its own convenience function
+        /**
+         * returns whether this `Message` is a `Response` with a `"result" = True` entry.
+         * this is needed so often that it deserves its own convenience function. This function is safe to be called
+         * even if this `Message` is no `Response` or does not have a `"result"` key.
+         * @return `true` if this `Message` is a `Response` with a `"result" = True` entry, `false` otherwise.
+         */
         bool isResultTrue(); //TODO: make const
 
-        bool isError() const; //returns whether the message is a response that is an error
+        /**
+         * returns whether this `Message` is a `Response` that is an `Error`
+         */
+        bool isError() const;
 
+        /**
+         * returns whether this `Message` is a `Requeset` that has the same `method` name as the provided `name` argument.
+         */
         bool hasMethodName(const char *name) const; //returns whether the message is a request and has the same method name as provided
 
+        /**
+         * convert the `jrpc::Message` object to a single-line `std::string` ready to be sent. For nicer formatting use `nlohmann::json::dump()` functionality and call `jrpc::Message::toJson().dump(4)` instead.
+         * @return
+         */
         std::string str() const;
     };
-
-    using MessageBatch = std::vector<Message>;
 
 }}
