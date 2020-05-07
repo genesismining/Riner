63e32c6fbdaddc899f595184bb2aa57722464e8a
Philipp Otterbein <philipp.otterbein@genesis-mining.com>
2020-05-07 13:10:27 +0000

    add documentation

--- src/util/LockUtils.h
+++ src/util/LockUtils.h
@@ -19,17 +19,20 @@ namespace riner {
     class UpgradeableLockGuarded;
 
 
-    template<class T, class Mutex = std::shared_timed_mutex>
+    template<class T, class SharedMutex = std::shared_timed_mutex>
     class WriteLocked;
 
-    template<class T, class Mutex = std::shared_timed_mutex>
+    template<class T, class SharedMutex = std::shared_timed_mutex>
     class ReadLocked;
 
     /**
-     * @brief return value of LockGuarded<V>'s lock() method
-     * Locked allows access to a V instance while ensuring that a certain mutex is locked.
-     * It's mainly used as the return type of Lockguarded<V>'s lock() method.
+     * @brief return type of LockGuarded<V>::lock() method
+     * Locked allows atomic access to an instance of V by locking a mutex.
+     * The lock is released upon the destruction of the Locked instance.
+     * It's mainly used as the return type of LockGuarded<V>::lock() method.
+     *
      * @tparam T the LockGuarded<V> that generated this instance via its lock() method
+     * @tparam Lock the underlying mutex ownership wrapper used to provide the locking functionality
      */
     template<class T, class Lock = std::unique_lock<typename T::mutex_type>>
     class Locked {
@@ -93,14 +96,16 @@ namespace riner {
         }
     };
 
+
     /**
-     * @brief Wrapper around an instance of T that is only accessible after acquiring a lock.
-     * LockGuarded contains a T instance t that is associated with a mutex. t is only accessible via the lock()
-     * method which returns an object of type Locked<...>. This Locked<...> object acts as a RAII-style lock guard, but also provides
-     * operator-> and operator* for accessing the T instance.
+     * @brief Wrapper around an instance t of T that is only exclusively and atomically accessible after acquiring a lock.
+     * LockGuarded contains an instance t that is associated with a mutex. t is only accessible via the object returned by the lock() medhod,
+     * which returns an object of type Locked<...>. This Locked<...> object acts as a RAII-style lock guard locking the mutex,
+     * but also provides operator-> and operator* for accessing the instance t.
      * This was inspired by lock_guard and the mutex<T> type of the rust language.
      *
      * @tparam T type of the instance which is to be protected by the mutex
+     * @tparam Mutex the underlying mutex implementation used to provide the locking functionality
      */
 
     template<class T, class Mutex>
@@ -148,6 +153,14 @@ namespace riner {
 
 
 
+    /**
+     * @brief Wrapper around an instance t of T which provides either exclusive read/write or shared read only access to the instance t
+     * The object returned by the lock() method acquires exclusive lock and t can be read and written through it.
+     * The method readLock() aquires a shared lock and t can be accessed read only through the returned object.
+     *
+     * @tparam T type of the instance which is to be protected by a shared mutex
+     * @tparam SharedMutex the underlying shared mutex implementation used to provide the locking functionality
+     */
     template<class T, class SharedMutex>
     class SharedLockGuarded : public LockGuarded<T, SharedMutex> {
 
@@ -171,25 +184,30 @@ namespace riner {
     };
 
 
-
-    template<class T, class Mutex>
-    class ReadLocked : public Locked<const UpgradeableLockGuarded<T, Mutex>, std::shared_lock<Mutex>> {
+    /**
+     * @brief Provides (shared) atomic, read only access to a wrapped instance of T
+     *
+     * @tparam T type of the instance which is to be protected by a shared mutex
+     * @tparam SharedMutex the underlying shared mutex implementation used to provide the locking functionality
+     */
+    template<class T, class SharedMutex>
+    class ReadLocked : public Locked<const UpgradeableLockGuarded<T, SharedMutex>, std::shared_lock<SharedMutex>> {
         template<class U, class M> friend class WriteLocked;
         template<class U, class M> friend class ImmediateLocked;
         template<class U, class M> friend class UpgradeableLockGuarded;
 
     protected:
 
-        ReadLocked(const UpgradeableLockGuarded<T, Mutex> &t)
-                : Locked<const UpgradeableLockGuarded<T, Mutex>, std::shared_lock<Mutex>>(t) {
+        ReadLocked(const UpgradeableLockGuarded<T, SharedMutex> &t)
+                : Locked<const UpgradeableLockGuarded<T, SharedMutex>, std::shared_lock<SharedMutex>>(t) {
         }
 
-        ReadLocked(Locked<UpgradeableLockGuarded<T, Mutex>, std::unique_lock<Mutex>> &&other)
-                : Locked<const UpgradeableLockGuarded<T, Mutex>, std::shared_lock<Mutex>>(std::move(other)) {
+        ReadLocked(Locked<UpgradeableLockGuarded<T, SharedMutex>, std::unique_lock<SharedMutex>> &&other)
+                : Locked<const UpgradeableLockGuarded<T, SharedMutex>, std::shared_lock<SharedMutex>>(std::move(other)) {
         }
 
-        ReadLocked(Locked<const UpgradeableLockGuarded<T, Mutex>, std::shared_lock<Mutex>> &&other)
-                : Locked<const UpgradeableLockGuarded<T, Mutex>, std::shared_lock<Mutex>>(std::move(other)) {
+        ReadLocked(Locked<const UpgradeableLockGuarded<T, SharedMutex>, std::shared_lock<SharedMutex>> &&other)
+                : Locked<const UpgradeableLockGuarded<T, SharedMutex>, std::shared_lock<SharedMutex>>(std::move(other)) {
         }
 
     public:
@@ -198,28 +216,41 @@ namespace riner {
         ReadLocked &operator=(ReadLocked &&) noexcept = default;
 
         /**
+         * @brief moves object
          * Dangerous: can cause a deadlock when used together with UpgradeableLockGuarded::lock()
+         *
+         * @return WriteLocked<T, SharedMutex> through which the instance t can be accessed read only
          */
-        WriteLocked<T, Mutex> upgrade() {
+        WriteLocked<T, SharedMutex> upgrade() {
             this->guarded.get().upgradeMut.lock();
             return std::move(*this);
         }
     };
 
-    template<class T, class Mutex>
-    class ImmediateLocked : public Locked<const UpgradeableLockGuarded<T, Mutex>, std::shared_lock<Mutex>> {
+    /**
+     * @brief Provides (shared) atomic, read only access to a wrapped instance of T, which can be upgraded atomically to (exclusive) read/write access
+     * Please note that instances of this class should be downgraded to read locks or upgraded to write locks as soon as possible
+     * because additional read locks cannot be aquired during the lifetime of this object.
+     * After requesting a write lock no other locks can be acquired, which means that the write lock will be
+     * acquired as soon as possible
+     *
+     * @tparam T type of the instance which is to be protected by a shared mutex
+     * @tparam SharedMutex the underlying shared mutex implementation used to provide the locking functionality
+     */
+    template<class T, class SharedMutex>
+    class ImmediateLocked : public Locked<const UpgradeableLockGuarded<T, SharedMutex>, std::shared_lock<SharedMutex>> {
         template<class U, class M> friend class ReadLocked;
         template<class U, class M> friend class WriteLocked;
         template<class U, class M> friend class UpgradeableLockGuarded;
 
     protected:
 
-        ImmediateLocked(const UpgradeableLockGuarded<T, Mutex> &t)
-                : Locked<const UpgradeableLockGuarded<T, Mutex>, std::shared_lock<Mutex>>(t) {
+        ImmediateLocked(const UpgradeableLockGuarded<T, SharedMutex> &t)
+                : Locked<const UpgradeableLockGuarded<T, SharedMutex>, std::shared_lock<SharedMutex>>(t) {
         }
 
-        ImmediateLocked(Locked<UpgradeableLockGuarded<T, Mutex>, std::unique_lock<Mutex>> &&other)
-                : Locked<const UpgradeableLockGuarded<T, Mutex>, std::shared_lock<Mutex>>(std::move(other)) {
+        ImmediateLocked(Locked<UpgradeableLockGuarded<T, SharedMutex>, std::unique_lock<SharedMutex>> &&other)
+                : Locked<const UpgradeableLockGuarded<T, SharedMutex>, std::shared_lock<SharedMutex>>(std::move(other)) {
         }
 
     public:
@@ -236,32 +267,48 @@ namespace riner {
             }
         }
 
-        WriteLocked<T, Mutex> upgrade() {
+        /**
+         * @brief moves object and atomically aquires unique access to the instance t
+         *
+         * @return WriteLocked<T, SharedMutex> through which the instance t can be read or written
+         */
+        WriteLocked<T, SharedMutex> upgrade() {
             return std::move(*this);
         }
 
-        ReadLocked<T, Mutex> downgrade() {
+        /**
+         * @brief moves object and acquires shared read only access to the instance t
+         * After calling the method other threads can get shared access to t again.
+         *
+         * @return ReadLocked<T, SharedMutex> through which the instance t can be read only
+         */
+        ReadLocked<T, SharedMutex> downgrade() {
             ImmediateLocked ret = std::move(*this);
             ret.guarded.get().upgradeMut.unlock();
             return std::move(ret);
         }
     };
 
-
-    template<class T, class Mutex>
-    class WriteLocked : public Locked<UpgradeableLockGuarded<T, Mutex>, std::unique_lock<Mutex>> {
+    /**
+     * @brief Provides (exclusive) atomic, read/write access to a wrapped instance of T, which can be downgraded atomically to (shared) read only access
+     *
+     * @tparam T type of the instance which is to be protected by a shared mutex
+     * @tparam SharedMutex the underlying shared mutex implementation used to provide the locking functionality
+     */
+    template<class T, class SharedMutex>
+    class WriteLocked : public Locked<UpgradeableLockGuarded<T, SharedMutex>, std::unique_lock<SharedMutex>> {
         template<class U, class M> friend class ReadLocked;
         template<class U, class M> friend class ImmediateLocked;
         template<class U, class M> friend class UpgradeableLockGuarded;
 
     protected:
 
-        WriteLocked(UpgradeableLockGuarded<T, Mutex> &t)
-                : Locked<UpgradeableLockGuarded<T, Mutex>, std::unique_lock<Mutex>>(t) {
+        WriteLocked(UpgradeableLockGuarded<T, SharedMutex> &t)
+                : Locked<UpgradeableLockGuarded<T, SharedMutex>, std::unique_lock<SharedMutex>>(t) {
         }
 
-        WriteLocked(Locked<const UpgradeableLockGuarded<T, Mutex>, std::shared_lock<Mutex>> &&other)
-                : Locked<UpgradeableLockGuarded<T, Mutex>, std::unique_lock<Mutex>>(std::move(other)) {
+        WriteLocked(Locked<const UpgradeableLockGuarded<T, SharedMutex>, std::shared_lock<SharedMutex>> &&other)
+                : Locked<UpgradeableLockGuarded<T, SharedMutex>, std::unique_lock<SharedMutex>>(std::move(other)) {
         }
 
     public:
@@ -278,14 +325,28 @@ namespace riner {
             }
         }
 
-        ReadLocked<T, Mutex> downgrade() {
-            ReadLocked<T, Mutex> ret = std::move(*this); // shared lock is owned now
+        /**
+         * @brief moves object and downgrades exclusive read/write access to shared read only access
+         *
+         * @return ReadLocked<T, SharedMutex> through which the instance t can be accessed read only
+         */
+        ReadLocked<T, SharedMutex> downgrade() {
+            ReadLocked<T, SharedMutex> ret = std::move(*this); // shared lock is owned now
             ret.guarded.get().upgradeMut.unlock();
             return ret;
         }
     };
 
-
+    /**
+     * @brief Wrapper around an instance t of T which provides either exclusive read/write or shared read only access to the instance t
+     * A similar class is SharedLockGuarded, but this class allows atomic upgrades or downgrades of locks, e.g.
+     * from shared read only access to exclusive read/write access. It has more overhead than the former class because
+     * it uses an additional mutex. Furthermore there are more guarantees that queued exclusive locks will be acquired
+     * before queued shared locks.
+     *
+     * @tparam T type of the instance which is to be protected by a shared mutex
+     * @tparam SharedMutex the underlying shared mutex implementation used to provide the locking functionality
+     */
     template<class T, class SharedMutex>
     class UpgradeableLockGuarded {
         template<class U, class M> friend class Locked;
@@ -310,6 +371,12 @@ namespace riner {
                 : t(std::forward<Args>(args) ...) {
         }
 
+        /**
+         * @brief aquires exclusive write access to t, which can be downgraded to shared read only access later
+         * For details: refer to WriteLocked
+         *
+         * @return WriteLocked<T, SharedMutex> through which the instance t can be accessed
+         */
         WriteLocked<T, SharedMutex> lock() const {
             upgradeMut.lock();
             return {*this};
@@ -320,6 +387,12 @@ namespace riner {
             return {*this};
         }
 
+        /**
+         * @brief aquires shared read only access to t, which can be upgraded to exlusive read/write access later
+         * For details: refer to ImmediateLocked
+         *
+         * @return ImmediateLocked<T, SharedMutex> through which the instance t can be accessed
+         */
         ImmediateLocked<T, SharedMutex> immediateLock() const {
             upgradeMut.lock();
             return {*this};
@@ -330,6 +403,11 @@ namespace riner {
             return {*this};
         }
 
+        /**
+         * @brief aquires shared read only access to t
+         *
+         * @return ReadLocked<T, SharedMutex> through which the instance t can be accessed
+         */
         ReadLocked<T, SharedMutex> readLock() const {
             std::lock_guard<std::mutex> lk(upgradeMut);
             return {*this};
